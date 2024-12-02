24efbc9bb7e7ca33704f8b8f4df113fcd40f8a56
mubasshirMurshed <mubmurshed@gmail.com>
2024-12-03 01:40:42 +1100

    Added day 3

---
+++ 2024/Day03/day03.py
@@ -0,0 +1,22 @@
+import sys
+
+def part1() -> None:
+    # Read input file
+    with open(sys.argv[1], 'r') as f:
+        lines = f.readlines()
+
+    pass
+
+
+def part2() -> None:
+    # Read input file
+    with open(sys.argv[1], 'r') as f:
+        lines = f.readlines()
+
+    pass
+
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
