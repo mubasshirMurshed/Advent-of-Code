8aa78f63bc0f274838c76cbb81066db8607afb87
mubasshirMurshed <mubmurshed@gmail.com>
2024-12-03 16:40:27 +1100

    Day 3 solutions

--- 2024/Day03/day03.py
+++ 2024/Day03/day03.py
@@ -1,10 +1,26 @@
 import sys
+import re
 
 def part1() -> None:
     # Read input file
     with open(sys.argv[1], 'r') as f:
         lines = f.readlines()
+    memory = ""
+    for line in lines:
+        memory += line
 
+    x = re.findall("mul\([0-9]+,[0-9]+\)", memory)
+    total = 0
+    for operation in x:
+        comma_idx = operation.find(",")
+        first_num = int(operation[4:comma_idx])
+        second_num = int(operation[comma_idx+1: len(operation)-1])
+        total += first_num*second_num
+
+        
+
+
+    print(total)
     pass
 
 
@@ -12,7 +28,42 @@ def part2() -> None:
     # Read input file
     with open(sys.argv[1], 'r') as f:
         lines = f.readlines()
+    memory = ""
+    for line in lines:
+        memory += line
 
+    muls = list(re.finditer("mul\([0-9]+,[0-9]+\)", memory))
+    enables = list(re.finditer("do\(\)", memory))
+    disables = list(re.finditer("don't\(\)", memory))
+    
+
+    instructions = [0]*len(memory)
+    for a in muls:
+        instructions[a.span()[0]] = a.group()
+
+    for a in enables:
+        instructions[a.span()[0]] = 1
+
+    for a in disables:
+        instructions[a.span()[0]] = 2
+
+
+    total = 0
+    enabled = True
+    for operation in instructions:
+        if operation == 0:
+            continue
+        elif operation == 1:
+            enabled = True
+        elif operation == 2:
+            enabled = False
+        else:
+            if enabled:
+                comma_idx = operation.find(",")
+                first_num = int(operation[4:comma_idx])
+                second_num = int(operation[comma_idx+1: len(operation)-1])
+                total += first_num*second_num
+    print(total)
     pass
 
 
