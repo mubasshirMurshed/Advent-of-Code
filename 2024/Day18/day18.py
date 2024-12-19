39a39c57b7e2d4305e7a63c576a0bdc2237c3205
mubasshirMurshed <mubmurshed@gmail.com>
2024-12-19 17:13:06 +1100

    Completed both Day 18 and Day 19 challenges

--- 2024/Day18/day18.py
+++ 2024/Day18/day18.py
@@ -1,20 +1,106 @@
 import sys
+from collections import deque
+
 
 def part1() -> None:
     # Read input file
     with open(sys.argv[1], 'r') as f:
         lines = f.readlines()
     
+    height = int(sys.argv[2])
+    width = int(sys.argv[2])
+    drops = [list(map(int, line.strip().split(","))) for line in lines]
 
+    # Make grid
+    grid = [['.' for _ in range(width)] for _ in range(height)]
+
+    # Add the first 1024 bytes
+    for i in range(1024):
+        x, y = drops[i]
+        grid[y][x] = '#'
+
+    # Find shortest path to end using bfs at (0, 0)
+    directions = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0]
+    ]
+    start = (0, 0)
+    visited = set()
+    visited.add(start)
+    distances = dict()
+    queue = deque()
+    queue.append(start)
+    distances[start] = 0
+    def inBounds(x, y):
+        return 0 <= x < width and 0 <= y < height
+    while len(queue) > 0:
+        u = queue.popleft()
+        for d in directions:
+            v = (u[0] + d[0], u[1] + d[1])
+            if v not in visited and inBounds(*v) and grid[v[1]][v[0]] == '.':
+                distances[v] = distances[u] + 1
+                visited.add(v)
+                queue.append(v)
+    
+    print(distances[(width-1, height-1)])
     pass
-
-
 
 
 def part2() -> None:
+    # Read input file
+    with open(sys.argv[1], 'r') as f:
+        lines = f.readlines()
     
-    pass
+    height = int(sys.argv[2])
+    width = int(sys.argv[2])
+    drops = [list(map(int, line.strip().split(","))) for line in lines]
 
+    # Make grid
+    grid = [['.' for _ in range(width)] for _ in range(height)]
+
+    # Add the first 1024 bytes
+    for i in range(1024):
+        x, y = drops[i]
+        grid[y][x] = '#'
+
+    # Find shortest path to end using bfs at (0, 0)
+    directions = [
+        [0, 1],
+        [0, -1],
+        [1, 0],
+        [-1, 0]
+    ]
+
+    # Helper function
+    def inBounds(x, y):
+        return 0 <= x < width and 0 <= y < height
+
+    for i in range(1024, len(drops)):
+        x, y = drops[i]
+        grid[y][x] = '#'
+        start = (0, 0)
+        visited = set()
+        visited.add(start)
+        distances = dict()
+        queue = deque()
+        queue.append(start)
+        distances[start] = 0
+        
+        while len(queue) > 0:
+            u = queue.popleft()
+            for d in directions:
+                v = (u[0] + d[0], u[1] + d[1])
+                if v not in visited and inBounds(*v) and grid[v[1]][v[0]] == '.':
+                    distances[v] = distances[u] + 1
+                    visited.add(v)
+                    queue.append(v)
+        
+        if (width-1, height-1) not in distances:
+            print(x,y)
+            break
+    pass
 
 if __name__ == "__main__":
     part1()
--- 2024/Day19/day19.py
+++ 2024/Day19/day19.py
@@ -1,18 +1,82 @@
 import sys
+from tqdm import tqdm
 
 def part1() -> None:
     # Read input file
     with open(sys.argv[1], 'r') as f:
         lines = f.readlines()
     
+    towels = lines[0].strip().split(", ")
+    designs = [line.strip() for line in lines[2:]]
+    results = dict()
 
+    def isPossible(design, idx):
+        if (design, idx) in results:
+            return results[(design, idx)]
+        
+        # Determine if design[idx:] is possible
+        if idx == len(design):
+            results[(design, idx)] = True
+            return results[(design, idx)]
+        
+        for towel in towels:
+            if idx == 0 and towel == design:
+                continue
+            
+            if idx + len(towel) <= len(design) and towel == design[idx:idx + len(towel)]:
+                if isPossible(design, idx + len(towel)):
+                    results[(design, idx)] = True
+                    return results[(design, idx)]
+        
+        results[(design, idx)] = False
+        return results[(design, idx)]
+    
+    # Filter towels that can already be made of smaller towels (optimisation)
+    new_towels = []
+    for towel in towels:
+        if not isPossible(towel, 0):
+            new_towels.append(towel)
+    towels = new_towels
+
+    count = 0
+    for design in tqdm(designs):
+        if isPossible(design, 0):
+            count += 1
+
+    print(count)
     pass
 
 
-
-
 def part2() -> None:
+    # Read input file
+    with open(sys.argv[1], 'r') as f:
+        lines = f.readlines()
     
+    towels = lines[0].strip().split(", ")
+    designs = [line.strip() for line in lines[2:]]
+    results = dict()
+
+    def isPossible(design, idx):
+        if (design, idx) in results:
+            return results[(design, idx)]
+        
+        # Determine number of possible ways to make design[idx:]
+        if idx == len(design):
+            # Base case, only 1 way to make empty string
+            results[(design, idx)] = 1
+        else:
+            # Start with 0 ways
+            results[(design, idx)] = 0
+            for towel in towels:
+                if idx + len(towel) <= len(design) and towel == design[idx:idx + len(towel)]:
+                    results[(design, idx)] += isPossible(design, idx + len(towel))
+        return results[(design, idx)]
+    
+    count = 0
+    for design in tqdm(designs):
+        count += isPossible(design, 0)
+
+    print(count)
     pass
 
 
